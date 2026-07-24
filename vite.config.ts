a496d89de72327546095da93187357376a74d7ea
smit244 <smitmandaliya24@gmail.com>
2026-07-24 23:41:08 +0530

    Auto-backup: 24-07-2026 23:41:08.28

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
